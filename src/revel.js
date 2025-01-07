ce102493b47ac33c67b244d00a818d68075840f3
DiegoNotreve <notreveogeide@gmail.com>
2025-01-07 00:45:46 -0300

    mudanças finais

---
+++ src/revel.js
@@ -0,0 +1,26 @@
+window.revel = ScrollReveal({ reset: true });
+
+revel.reveal(".efeito-logo", { duration: 2000, distance: "-100px" });
+revel.reveal(".efeito-menu", { duration: 2000, distance: "-100px" });
+revel.reveal(".menua", {duration: 2000, distance: "100px"})
+
+revel.reveal(".efeito-barber", { duration: 2000, distance: "-200px"});
+revel.reveal(".efeito-bololo", { duration: 2000, distance: "200px"});
+revel.reveal(".efeito-pole1", { duration: 2000, distance: "-200px", delay: 500, origin: 'left'});
+revel.reveal(".efeito-pole2", { duration: 2000, distance: "-200px", delay: 500, origin: 'right'});
+
+revel.reveal(".efeito-central", {duration:2000, scale: 0.85, delay: 500});
+revel.reveal(".efeito-sobre", { duration: 2000, distance: "-100px", delay: 600});
+revel.reveal(".efeito-retrato", { duration: 2000, distance: "-200px", delay: 800,});
+
+revel.reveal(".efeito-servico", { duration: 2000, distance: "-200px"});
+revel.reveal(".efeito-primeiro", { duration: 2000, distance: "200px"});
+revel.reveal(".efeito-servicos", { duration: 2000, distance: "-200px"});
+revel.reveal(".efeito-info", { duration: 2000, distance: "-200px", delay: 500, origin: 'right'});
+revel.reveal(".efeito-mapa", { duration: 2000, distance: "-200px", delay: 500, origin: 'left'});
+revel.reveal(".efeito-fraselocal", { duration: 2000, distance: "200px"});
+
+
+
+revel.reveal(".", {duration: 2000, distance: "400px", origin: 'left'})
+
